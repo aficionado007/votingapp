e34a83dff0867f87238b9eb528386a1092958b4a
aficionado007 <geetikabakshi97@gmail.com>
2017-09-30 17:44:33 +0530

    Update models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -9,6 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
     	return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
