dba24d6ae5f60140f0fdce2bb436b52ac948e8ac
aficionado007 <geetikabakshi97@gmail.com>
2017-09-30 17:58:28 +0530

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from .models import Question,Choice
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from django.urls import reverse
 from django.views import generic
 
@@ -26,18 +26,17 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+    def vote(request, question_id):
+    	question = get_object_or_404(Question, pk=question_id)
+    	try:
+        	selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    	except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        	return render(request, 'polls/detail.html', {
+            	'question': question,
+            	'error_message': "You didn't select a choice.",
+        	})
+    	else:
+        	selected_choice.votes += 1
+        	selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
